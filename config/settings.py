3b2dfccf2276539823e555735e6d861db300e288
Dinmukhamet <igissinov_d@auca.kg>
2023-02-25 16:29:24 +0600

    feat: setup djoser

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "restaurant",
-    "rest_framework"
+    "rest_framework",
+    "djoser",
+    "rest_framework.authtoken"
 ]
 
 MIDDLEWARE = [
@@ -115,3 +117,13 @@ STATIC_ROOT = "/var/www/little-lemon/static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
+
+DJOSER = {"USER_ID_FIELD": "username"}
