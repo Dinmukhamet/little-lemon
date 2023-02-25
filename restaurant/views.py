d2e440c25a2edec013e3fd2fe3a0beadc9aa0959
Dinmukhamet <igissinov_d@auca.kg>
2023-02-25 16:40:33 +0600

    feat: secured endpoints with IsAuthenticated permission

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,18 +1,22 @@
 from rest_framework import generics, viewsets
 from .models import Booking, Menu
 from .serializers import BookingSerializer, MenuSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
